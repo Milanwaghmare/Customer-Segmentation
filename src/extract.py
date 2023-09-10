6ce4fc998da7d61b7d939033447a3659d0d2054e
Milan Waghmare <milanwaghmare84@gmail.com>
2023-09-10 20:40:33 +0200

    extract-transform

--- src/extract.py
+++ src/extract.py
@@ -2,6 +2,7 @@ from typing import Iterator
 
 import psycopg2
 import pandas as pd
+import numpy as np
 
 def connect_to_redshift(dbname, host, port, user, password):
     """Method that connects to redshift. This gives a warning so will look for another solution"""
@@ -62,49 +63,3 @@ def extract_customer_data(dbname,host,port,user,password):
 
     return online_trans_cleaned
 
-def calculate_recency(customer_data):
-    customer_data["recency"] = round((customer_data['recent_purchase_made'].max() - customer_data['recent_purchase_made']) / np.timedelta64(1, 'D'),0)
-    print("Recency:", customer_data["recency"])
-
-
-def calculate_customer_frequency(customer_data):
-    # Calculate frequency: how often a customer has bought products
-    frequency_data = customer_data.groupby('customer_id')['stock_code'].count().reset_index()
-
-    # Rename the column 'stock_code' as 'frequency'
-    frequency_data.rename(columns={'stock_code': 'frequency'}, inplace=True)
-
-    # Merge with customer_data using a left join
-    customer_freq_data = customer_data.merge(frequency_data, on='customer_id', how='left')
-
-    return customer_freq_data
-
-def calculate_monetary_value(customer_data):
-    # Calculate Monetary Value: sum of avg_order_value for each customer
-    monetary_data = customer_data.groupby('customer_id')['avg_order_value'].sum().reset_index()
-
-    # Rename the columns
-    monetary_data.rename(columns={'avg_order_value': 'monetary_value'}, inplace=True)
-
-    # Merge with customer_data using a left join
-    customer_final_data = customer_data.merge(monetary_data, on='customer_id', how='left')
-
-    return customer_final_data
-
-
-def calculate_scores(customer_final_data):
-    # Binning and scoring for recency
-    customer_final_data['rec_score'] = pd.qcut(customer_final_data['recency'], 4, ['4', '3', '2', '1'])
-    # Binning and scoring for frequency
-    customer_final_data['freq_score'] = pd.qcut(customer_final_data['frequency'], 4, ['4', '3', '2', '1'])
-    # Binning and scoring for monetary value
-    customer_final_data['mone_score'] = pd.qcut(customer_final_data['monetary_value'], 4, ['4', '3', '2', '1'])
-
-
-# Calculate RFM Score
-customer_final_data['rfm_score'] = customer_final_data.rec_score + customer_final_data.freq_score + customer_final_data.mone_score
-
-# Assigned variable name as segment_labels
-segment_labels = ['low-value', 'mid-value', 'high-value']
-# Using pd.qcut created new column name value_segment
-customer_final_data['value_segment'] = pd.qcut(customer_final_data['rfm_score'], q=3, labels=segment_labels)
